4c539c0190e5763f28ac4ee1cb0c90f7f8a9806a
Marius <mariushannappel@gmail.com>
2023-09-08 10:33:26 +0200

    Spring Boot Projekt erstellt

---
+++ src/main/java/de/destatis/KlausurplanerBackend/KlausurplanerBackendApplication.java
@@ -0,0 +1,13 @@
+package de.destatis.KlausurplanerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlausurplanerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlausurplanerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/destatis/KlausurplanerBackend/KlausurplanerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.destatis.KlausurplanerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlausurplanerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
