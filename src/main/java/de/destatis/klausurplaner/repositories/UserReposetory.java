907c611847cf7d5905adef5bdeb608e0e88e0099
Kevin Mittag <Kevin.Mittag@destatis.de>
2023-09-25 10:56:31 +0200

    .

--- src/main/java/de/destatis/klausurplaner/repositories/UserReposetory.java
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // Define custom query methods here if needed
-}
