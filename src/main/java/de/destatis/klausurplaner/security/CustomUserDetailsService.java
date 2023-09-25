d62063e3b92e9e06cc4512e3cc629c2339656f09
Marius <79973334+Marius1211@users.noreply.github.com>
2023-09-25 21:43:10 +0200

    SecurityConfig geupdated AuthController, RegisterDto, CUDS hinzugefügt

---
+++ src/main/java/de/destatis/klausurplaner/security/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package de.destatis.klausurplaner.security;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import de.destatis.klausurplaner.entities.Role;
+import de.destatis.klausurplaner.entities.UserEntity;
+import de.destatis.klausurplaner.repositories.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username).orElseThrow(()-> new UsernameNotFoundException("Nutzername nicht gefunden"));
+        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+    
+    private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    }
+
+}
--- src/main/java/de/destatis/klausurplaner/security/SecurityConfig.java
+++ src/main/java/de/destatis/klausurplaner/security/SecurityConfig.java
@@ -1,35 +1,51 @@
 package de.destatis.klausurplaner.security;
 
 import org.h2.engine.UserBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
-/* 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;*/
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-     /*
+     
+    private CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http
-            
-            .authorizeHttpRequests()
-				.anyRequest().authenticated()
-			.and()
-			.formLogin((form) -> form
-				.loginPage("http://localhost:8080/login")
-				.permitAll()
-			)
-			.logout((logout) -> logout.permitAll());
+        http
+            /*
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin((form) -> form
+                                .loginPage("http://localhost:8080/login")
+                                .permitAll()
+                )
+                .logout((logout) -> logout.permitAll())
+                .httpBasic();
 
             /*
             .csrf().disable()
@@ -38,18 +54,18 @@ public class SecurityConfig {
 			.and()
             .httpBasic();
             */
-            /*s
+            
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/", "/home").permitAll()
+				.requestMatchers(HttpMethod.GET).permitAll()
 				.anyRequest().authenticated()
-			)
+			);
+            /*
 			.formLogin((form) -> form
 				.loginPage("/login")
 				.permitAll()
 			)
 			.logout((logout) -> logout.permitAll());
             */
-/*
 		return http.build();
 	}
     
@@ -69,5 +85,17 @@ public class SecurityConfig {
 
             return new InMemoryUserDetailsManager(admin, user);
     }
-    */
+    
+    @Bean
+    public AuthenticationManager authentificationManager(
+        AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
